53ffb6ac93546fbdea118d0ace61681c7239bf4e
fernandobouchet <fernandobouchet@gmail.com>
2022-03-28 21:22:50 -0300

    Show a message when no favorites movies are selected

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -2,6 +2,7 @@ import Cards from "../Cards/Cards";
 import { nanoid } from "nanoid";
 import styled from "styled-components";
 import GoToTopButton from "../Button/GoToTop/GoToTopButton";
+import { BsTypeH1 } from "react-icons/bs";
 
 function Favorites(props) {
   const { favs } = props;
@@ -12,8 +13,16 @@ function Favorites(props) {
 
   return (
     <>
-      <CardsContainer>{Movies}</CardsContainer>
-      <GoToTopButton />
+      {favs.length >= 1 ? (
+        <>
+          <CardsContainer>{Movies}</CardsContainer>
+          <GoToTopButton />
+        </>
+      ) : (
+        <MessageContainer>
+          <StyledH2>You don't have favorites movies added yet.</StyledH2>
+        </MessageContainer>
+      )}
     </>
   );
 }
@@ -32,4 +41,14 @@ const CardsContainer = styled.div`
     padding-top: 5rem;
     grid-template-columns: repeat(2, 16rem);
   }
+`;
+const MessageContainer = styled.div`
+  padding-top: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledH2 = styled.h2`
+  color: white;
 `;
